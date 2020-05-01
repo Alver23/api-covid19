2e5895b547b4bc94483ca048015ba795d97d1021
Alver Grisales <alexander.grisales@almundo.com>
2020-05-01 02:08:22 -0500

    refactor: removed endpoints old

--- src/api/covid/covid.controller.ts
+++ src/api/covid/covid.controller.ts
@@ -4,23 +4,23 @@ import { Controller, Get } from '@nestjs/common';
 // Services
 import { CovidService } from './services/covid.service';
 
-@Controller('covid')
+@Controller('cases')
 export class CovidController {
   constructor(
     private readonly covidService: CovidService,
   ) {}
 
-  @Get('cases')
+  @Get()
   public async getCases() {
     return this.covidService.getCases();
   }
 
-  @Get('cases/recovered')
+  @Get('recovered')
   public async getCasesRecoved() {
     return this.covidService.getCasesRecovered();
   }
 
-  @Get('cases/deaths')
+  @Get('deaths')
   public async getCasesDeaths() {
     return this.covidService.getCasesDeaths();
   }
